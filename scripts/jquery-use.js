e0170a68d555c297c46be69c015dbc1a53336dc7
Harmeet Singh <harmeet4855@gmail.com>
2016-10-04 12:51:13 +0530

    Adding About

--- scripts/jquery-use.js
+++ scripts/jquery-use.js
@@ -2,16 +2,30 @@
 $( document ).ready(function() {
     setTimeout(function(){ 
         $("#visible-menu").click(function(){
-            $("md-tabs-wrapper").fadeOut();
+            $("md-tabs-wrapper").slideUp();
             $("#visible-menu").fadeOut();
             $("#hidden-menu").fadeIn();
         });
 
         $("#hidden-menu").click(function(){
             // $(".menu-visibility").fadeIn();
-            $("md-tabs-wrapper").fadeIn();
+            $("md-tabs-wrapper").slideDown();
             $("#hidden-menu").fadeOut();
             $("#visible-menu").fadeIn();
         });
+
+        $("#visible-about").click(function(){
+            $("#profile-about").slideUp();
+            $("#visible-about").fadeOut();
+            $("#hidden-about").fadeIn();
+        });
+
+        $("#hidden-about").click(function(){
+            $("#profile-about").slideDown();
+            $("#hidden-about").fadeOut();
+            $("#visible-about").fadeIn();
+        });
+
+
     }, 2000);
 });
